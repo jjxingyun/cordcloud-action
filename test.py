01f11653b460421ca902dbeb7f9077fd37e135fd
xingyun <34016251+jjxingyun@users.noreply.github.com>
2025-06-21 11:16:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from app.action import Action
 
 email = 'contact@yanglibin.info'
 passwd = 'xxxxxxxx'
-host = 'cordcloud.us'
+host = 'cordcloud.one'
 action = Action(email, passwd, host=host)
 action.run()
